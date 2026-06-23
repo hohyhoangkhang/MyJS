76165d012defdeebe9e45484b16d94a8346a6f55
hohyhoangkhang <hoangkhang27062007@gmail.com>
2026-06-23 08:59:22 +0700

    Update control.js

--- js/control.js
+++ js/control.js
@@ -1,6 +1,4 @@
 let score = 7.2;
-
-// TODO 1: dùng if...else if...else để in ra xếp loại
 if (score >= 9) {
   console.log("Xuất sắc");
 } else if (score >= 7) {
@@ -10,15 +8,11 @@ if (score >= 9) {
 } else {
   console.log("Yếu");
 }
-
 console.log("In các số từ 1 đến 5 bằng vòng for:");
-// TODO 2: viết vòng for in các số từ 1 đến 5
 for (let i = 1; i <= 5; i++) {
   console.log(i);
 }
-
 let total = 0;
-// TODO 3: viết vòng for tính tổng từ 1 đến 5
 for (let i = 1; i <= 5; i++) {
   total += i;
 }
